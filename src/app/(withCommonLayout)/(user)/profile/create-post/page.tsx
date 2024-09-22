f88fd5055ff641438311925f0811d077e0850738
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-09-23 00:16:16 +0600

    all form done

--- src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
@@ -1,11 +1,15 @@
 "use client";
+import CustomCollapse from "@/src/components/Form/CustomCollapse";
+import CustomCollapseMultiple from "@/src/components/Form/CustomCollapseMultiple";
 import CustomDynamicDoubleInput from "@/src/components/Form/CustomDynamicDoubleInput";
 import CustomDynamicInput from "@/src/components/Form/CustomDynamicInput";
 import CustomFileUpload from "@/src/components/Form/CustomFileUpload";
 import CustomForm from "@/src/components/Form/CustomForm";
+import CustomRangePicker from "@/src/components/Form/CustomRangePicker";
 import CustomReactQuill from "@/src/components/Form/CustomReactQuill";
 import CustomSelect from "@/src/components/Form/CustomSelect";
 import CustomSelectWithWatch from "@/src/components/Form/CustomSelectWithWatch";
+import CustomTimePicker from "@/src/components/Form/CustomTimePicker";
 import CustomToggle from "@/src/components/Form/CustomToggle";
 import { Button } from "@nextui-org/button";
 import React, { useState } from "react";
@@ -14,11 +18,14 @@ import { FieldValues, SubmitHandler } from "react-hook-form";
 const createPostPage = () => {
   const [imageFiles, setImageFiles] = useState([]);
   const [multiSelectWatch, setMultiSelectWatch] = useState(null);
+  const [issFiltersSelected, setIsFiltersSelected] = useState(null);
+  const [timeRange, setTimeRange] = useState(null);
+  const [time, setTime] = useState(null);
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     console.log(data);
   };
 
-  console.log(multiSelectWatch);
+  console.log(timeRange);
 
   return (
     <>
@@ -80,6 +87,32 @@ const createPostPage = () => {
             option={["question", "answer"]}
           />
 
+          <CustomCollapse
+            label="Availability"
+            name="availability"
+            changeOnValue={setIsFiltersSelected}
+            options={[
+              { label: "isStock", value: "isStock" },
+              { label: "stockOut", value: "stockOut" },
+            ]}
+          />
+          <CustomCollapseMultiple
+            label="dummy"
+            name="dummy"
+            changeOnValue={setIsFiltersSelected}
+            options={[
+              { label: "isStock", value: "isStock" },
+              { label: "stockOut", value: "stockOut" },
+            ]}
+          />
+
+          <CustomRangePicker
+            name="dateRange"
+            label="Select Date Range"
+            changeOnValue={setTimeRange}
+          />
+
+          <CustomTimePicker name="time" label="Select Time" />
           <Button type="submit">Submit</Button>
         </CustomForm>
       </div>
--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -72,7 +72,7 @@ export const Navbar = () => {
 
         <NavbarMenu>
           <div className="mx-4 mt-2 flex flex-col gap-2">
-            {siteConfig.navMenuItems.map((item, index) => (
+            {siteConfig?.navMenuItems?.map((item, index) => (
               <NavbarMenuItem key={`${item}-${index}`}>
                 <Link
                   color={
